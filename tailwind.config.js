97e52d76efe73578bd5a9949f4aacb3242b12249
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-02-14 08:52:38 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
     extend: {
-      screen: {
+      screens: {
         "xs": "320px",
       }
     },
