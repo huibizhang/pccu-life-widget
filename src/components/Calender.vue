9468f02feff8859f4ab0c910b8e0387b80d8f09b
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-02-17 08:57:27 +0800

    fix: whitespace

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -3,10 +3,10 @@
     class="max-w-[350px] bg-orange-600 text-white font-bold rounded-lg select-none flex flex-col space-y-3 items-center p-3"
   >
     <div>
-      {{`星期${week}`}}
+      {{ `星期${week}` }}
     </div>
     <div class="text-5xl font-black">
-      {{dd}}
+      {{ dd }}
     </div>
     <div>
       {{ `${yy} 年 ${MM} 月` }}
@@ -34,18 +34,18 @@ export default {
 
       this.yy = this.time.getFullYear();
       this.MM = [
-          "一",
-          "二",
-          "三",
-          "四",
-          "五",
-          "六",
-          "七",
-          "八",
-          "九",
-          "十",
-          "十一",
-          "十二",
+        "一",
+        "二",
+        "三",
+        "四",
+        "五",
+        "六",
+        "七",
+        "八",
+        "九",
+        "十",
+        "十一",
+        "十二",
       ][this.time.getMonth()];
       this.dd = this.time.getDate();
 
